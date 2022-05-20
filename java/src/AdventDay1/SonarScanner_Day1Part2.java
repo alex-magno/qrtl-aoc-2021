1381b5ae05a368697a3772b26ac62f7d591487c1
Pedro <pedrohrl.123@gmail.com>
2022-05-20 13:07:48 -0300

    Corrections to reduce input file redundancies, as well as removal of IDE specific folders and the output folder from java compiler. Added .gitignore file on the root of folder

--- java/src/AdventDay1/SonarScanner_Day1Part2.java
+++ java/src/AdventDay1/SonarScanner_Day1Part2.java
@@ -1,29 +1,26 @@
 package AdventDay1;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 
-public class SonarScanner_Day1 {
+public class SonarScanner_Day1Part2 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(new FileReader("../challenges/day1/data/input.txt"));
         int[] depthDataArray = new int[2000];
         int i = 0;
         int increasedMeasurements = 0;
 
-
         while (scanner.hasNextInt()) {
 
             depthDataArray[i++] = scanner.nextInt();
         }
 
-        for (int counter = 0; counter < (depthDataArray.length - 1); counter++) {
+        for (int counter = 0; counter < (depthDataArray.length - 3); counter++) {
 
-            if (depthDataArray[counter] < depthDataArray[counter + 1]) {
+            if (depthDataArray[counter] + depthDataArray[counter + 1] + depthDataArray[counter + 2] < depthDataArray[counter + 1] + depthDataArray[counter + 2] + depthDataArray[counter + 3]) {
                 increasedMeasurements++;
             }
 
@@ -31,5 +28,3 @@ public class SonarScanner_Day1 {
         System.out.println(increasedMeasurements);
     }
 }
-
-
