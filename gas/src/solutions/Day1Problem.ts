5a0310fa105e1fd8e948244b0a921dfdfe9d5737
victordrumond <victordrumond@hotmail.com>
2022-05-19 22:06:08 -0300

    Add day 1 solutions

--- gas/src/solutions/Day1Problem.ts
+++ gas/src/solutions/Day1Problem.ts
@@ -2,18 +2,44 @@ namespace challenges {
 
     export class day1Problem {
 
-        data: any[];
+        data: string[];
 
-        constructor() {
-            this.data = [];
+        constructor(dataUrl: string) {
+            this.data = this.getData(dataUrl);
         }
 
         solve(): Solution {
-            let count = 0
+            const partOneResult = this.solvePartOne(this.data);
+            const partTwoResult = this.solvePartTwo(this.data);
             return {
-                challenge: 'd1',
-                result: `<p>Result: ${count}</p>`
+                day: 'd1',
+                partOneResult: `<p>Part One result: ${partOneResult}</p>`,
+                partTwoResult: `<p>Part Two result: ${partTwoResult}</p>`
             }
+        }
+
+        protected solvePartOne(data: any[]): number {
+            let counter = 0;
+            for (let i = 0; i < data.length; i++) {
+                if (+data[i] > +data[i - 1]) {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        protected solvePartTwo(data: any[]): number {
+            let newData: any[] = [];
+            for (let i = 0; i < data.length; i++) {
+                const [a, b, c]: number[] = [+data[i], +data[i + 1], +data[i + 2]];
+                newData.push(a + b + c);
+            }
+            return this.solvePartOne(newData);
+        }
+
+        private getData(dataUrl: string): string[] {
+            const response = UrlFetchApp.fetch(dataUrl);
+            return response.getContentText().split('\n');
         }
     }
 }
